f48b1a6b6d40738e314ddd4062f516a51ba27016
Julio Gonzalez <jugonzalez@Julios-MacBook-Pro.local>
2025-08-28 17:08:37 -0500

    Initilize project microserviceCP

---
+++ src/main/java/com/exercise/microservicioCP/MicroservicioCpApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.microservicioCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/microservicioCP/MicroservicioCpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.microservicioCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
